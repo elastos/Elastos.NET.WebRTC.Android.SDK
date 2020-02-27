3a317c1e689d9f9e2565baa611e04cac3747bb25
Ping Chen <cpedia@gmail.com>
2020-02-27 10:16:24 +0800

    remove old demo app.

--- demo/ElastosWebrtcAndroidDemo/app/src/main/java/com/allcom/elastos_webrtc_android_demo/util/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.allcom.elastos_webrtc_android_demo.util;
-
-public class Constants {
-
-    public static final String CACHE_KEY_FRIENDS = "cache_key_friends";
-
-}
--- demo/ElastosWebrtcAndroidDemo/elastos-webrtc/src/main/java/com/allcom/elastos_webrtc/util/CandidateKey.java
+++
@@ -1,7 +0,0 @@
-package com.allcom.elastos_webrtc.util;
-
-public enum CandidateKey {
-    sdpMLineIndex,
-    sdpMid,
-    sdp,
-}
