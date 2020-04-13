2912a1899a6531099f062bfb20c50eec240ec39c
LEE <lixhuan822@gmail.com>
2020-04-13 10:23:51 +0800

    delete carrier

--- carrier/native-dist/lib/README.md
+++
@@ -1,7 +0,0 @@
-This folder is hold for libraries of following architectures also as directories:
-
-- armeabi-v7a
-- arm64-v8a
-- x86
-- x86_64
-
--- carrier/src/androidTest/java/org/elastos/carrier/robot/Robot.java
+++
@@ -1,6 +0,0 @@
-package org.elastos.carrier.robot;
-
-public class Robot {
-    public final static String ROBOTHOST = "192.168.0.107";
-    public final static String ROBOTPORT = "7237";
-}
